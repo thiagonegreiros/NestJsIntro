414e0e2915f710b3c3792c9ff667bfba57270c56
Thiago Negreiros (BSC - IT) <thiago_vieira@cal-comp.com.br>
2020-03-31 08:56:49 -0300

    Import Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+
+//Para instalar o Mongoose - npm install --save mongoose @nestjs/mongoose
+//Importando o mongoose
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/product.module';
 
+//Ele cria atutomaticamente o meu banco de dados nestjs-demo
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, MongooseModule.forRoot(
+      'mongodb+srv://negreiros:negreiros@cluster0-3ekcc.mongodb.net/nestjs-demo?retryWrites=true&w=majority'
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/product.model.ts
+++ src/products/product.model.ts
@@ -1,3 +1,14 @@
+//Importando todas as classes do mongoose
+import * as mongoose from 'mongoose';
+
+//Criando um Schema
+export const ProductSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+
+});
+
 export class Product
 {
     constructor
